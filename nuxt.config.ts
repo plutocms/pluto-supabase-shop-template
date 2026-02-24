8ef5bcc372afbb03476c49898eafc93bcf903a3e
Victor Ribeiro <devjvribeiro@gmail.com>
2026-02-24 12:42:04 -0300

    chore: removed `pluto` direct usage
    It is used by `supabase-shop` internally

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  extends: [
-    ["github:plutocms/pluto", { install: true }],
-    ["github:plutocms/supabase-shop", { install: true }],
-  ],
+  extends: [["github:plutocms/supabase-shop", { install: true }]],
 
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
